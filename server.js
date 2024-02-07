d22a63bed4517ce9598141174b2d314d03bff685
Juniper <juniperwrenarts@gmail.com>
2024-02-06 19:26:38 -0500

    setup MySQL connection and basic menu functionality.

---
+++ server.js
@@ -0,0 +1,135 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+
+// create a MySQL connection
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "employee_tracker",
+});
+
+// connect to the database
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("Connected to the database!");
+    // start the application
+    start();
+});
+
+// Function to start the application
+function start() {
+    inquirer
+        .prompt({
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Update an employee role",
+                // Add more choices if needed/wanted 
+                "Exit",
+            ],
+        })
+        .then((answer) => {
+            switch (answer.action) {
+                case "View all departments":
+                    viewAllDepartments();
+                    break;
+                case "View all roles":
+                    viewAllRoles();
+                    break;
+                case "View all employees":
+                    viewAllEmployees();
+                    break;
+                case "Add a department":
+                    addDepartment();
+                    break;
+                case "Add a role":
+                    addRole();
+                    break;
+                case "Add an employee":
+                    addEmployee();
+                    break;
+                case "Update an employee role":
+                    updateEmployeeRole();
+                    break;
+                // Add cases for bonus functionalities as needed
+                case "Exit":
+                    connection.end();
+                    console.log("Goodbye!");
+                    break;
+            }
+        });
+}
+
+// Function to view all departments
+function viewAllDepartments() {
+    const query = "SELECT * FROM department";
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        start();
+    });
+}
+
+// Function to view all roles
+function viewAllRoles() {
+    const query = "SELECT * FROM role";
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        start();
+    });
+}
+
+// Function to view all employees
+function viewAllEmployees() {
+    const query = "SELECT * FROM employee";
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        start();
+    });
+}
+
+// Function to add a department
+function addDepartment() {
+    inquirer
+        .prompt({
+            type: "input",
+            name: "name",
+            message: "Enter the name of the new department:",
+        })
+        .then((answer) => {
+            const query = "INSERT INTO department (name) VALUES (?)";
+            connection.query(query, [answer.name], (err, res) => {
+                if (err) throw err;
+                console.log(`Added department ${answer.name} to the database!`);
+                start();
+            });
+        });
+}
+
+// Function to add a role
+function addRole() {
+    // add functionality here
+}
+
+// Function to add an employee
+function addEmployee() {
+    // add functionality here
+}
+
+// Function to update an employee role
+function updateEmployeeRole() {
+    // add functionality here
+}
+
+// Additional functions for bonus functionalities can be added here
