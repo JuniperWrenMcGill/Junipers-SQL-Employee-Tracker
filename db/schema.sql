3c5e34d6f1070cc92dd22b9342f2b85f51852337
Juniper <juniperwrenarts@gmail.com>
2024-02-07 22:12:45 -0500

    debugged schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,8 @@
+DROP DATABASE IF EXISTS employee_tracker;
 CREATE DATABASE employee_tracker;
-DROP DATABASE employee_tracker;
- 
+
+USE employee_tracker;
+
 -- create department 
 CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
